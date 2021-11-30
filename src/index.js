15a2bb9889b92fe3e76468e84eb074a4f130f440
Moisés <moisesnetored@gmail.com>
2021-11-30 15:22:49 -0400

    feat: Requisitos para criar conta, biblioteca uuid adicionada ao projeto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const express = require("express");
+const {v4: uuidv4} = require("uuid");
 
-app = express();
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+/**
+ * cpf -> string
+ * name -> string
+ * id -> uuid
+ * statement (extrato / lançamentos) -> []
+ */
+
+app.post("/account", (request, response)=> {
+  const {cpf, name} = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
